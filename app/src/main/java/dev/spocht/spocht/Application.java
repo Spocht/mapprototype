137f2f4869e7eaec73c9519cc2e1cfa924d45bec
Beat Luginbühl <lugi@lugipfupf.ch>
2015-08-11 14:54:50 +0200

    remove api keys, move'em to the appropriate xml respectively

--- app/src/main/java/dev/spocht/spocht/Application.java
+++ app/src/main/java/dev/spocht/spocht/Application.java
@@ -4,6 +4,7 @@ package dev.spocht.spocht;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.StringDef;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -42,7 +43,7 @@ public class Application extends android.app.Application {
         super.onCreate();
         registerSubclasses();
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd", "I7uNfjct4uL5GMwC8kUiubofsWDVAmzG1CAf0VE0");
+        Parse.initialize(this, "@string/parse_application_id", "@string/parse_client_key");
 
     }
 
