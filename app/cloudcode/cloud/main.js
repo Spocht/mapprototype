2cf1aa811baee173649659c002f17a96404abc06
Dessler Markus, ENT-BAN-SOL-FSO-ADY-SWE <Markus.Dessler@swisscom.com>
2015-08-25 18:27:49 +0200

    LocationListener now sharable and new dummy function in main.js

--- app/cloudcode/cloud/main.js
+++ app/cloudcode/cloud/main.js
@@ -4,3 +4,18 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("spochtRokks", function(request, response) {
+	var query = new Parse.Query("MyUser");
+	query.equalTo("firstname", request.params.firstname);
+	query.find({
+		success: function(results) {
+			response.success(results);
+			//response.success(request.params.firstname);
+		},
+		error: function() {
+			response.error("nope");
+		}
+	});
+	
+});
