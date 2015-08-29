bfcab3c03eb9aa023d2a410f410f8d5da6b84e8e
Rudolf Müller <rudolfj.mueller@web.de>
2015-08-29 14:49:42 +0200

    Experience !! DataClasses (2)

--- app/src/main/java/dev/spocht/spocht/data/Experience.java
+++ app/src/main/java/dev/spocht/spocht/data/Experience.java
@@ -11,5 +11,55 @@ import com.parse.ParseQuery;
  */
 @ParseClassName("Experience")
 public class Experience extends ParseData {
-
+    public Experience()
+    {//default constructor for Parse.com
+        ;
+    }
+    protected void setXP(final int val)
+    {
+        put("xp",val);
+    }
+    public int xp()
+    {
+        int xp = getInt("xp");
+        if(xp < 0)
+        {
+            xp = 0;
+        }
+        return (xp);
+    }
+    protected void setLevel(final int lvl)
+    {
+        put("level",lvl);
+    }
+    public int level()
+    {
+        int lvl = getInt("level");
+        if(lvl < 1)
+        {
+            lvl = 1;
+        }
+        return (lvl);
+    }
+    public int xpForNextLevel()
+    {
+        return((int)Math.pow(10,level()));
+    }
+    public void addXP(final int val)
+    {
+        int xpNew = val;
+        if(xpNew < 0)
+        {
+            xpNew = xp();
+        }
+        else
+        {
+            xpNew += xp();
+        }
+        setXP(xpNew);
+        if(xpNew > xpForNextLevel())
+        {
+            setLevel(level()+1);
+        }
+    }
 }
