b0b8cd102dca51d3153f9de82eec45fd6669af7b
Rudolf Müller <rudolfj.mueller@web.de>
2015-09-05 13:46:01 +0200

    implement geofence in EventMonitor with switch case => evaluate proper implementation with States and as a seperate module

--- app/src/main/java/dev/spocht/spocht/data/EventMonitor.java
+++ app/src/main/java/dev/spocht/spocht/data/EventMonitor.java
@@ -17,25 +17,72 @@ import dev.spocht.spocht.location.MyLocationListener;
  * Created by edm on 26.08.15.
  */
 public class EventMonitor {
-
-
     private Event mEvent=null;
-    private double maxDistanceToEventInKilometers = 0.2;
+    private enum EventMonitorState{
+        IN_RANGE (0.21),
+        OUT_RANGE (0.19);
+
+        private final double maxDistanceToEventInKilometers;
+        private EventMonitorState(final double distance)
+        {
+            maxDistanceToEventInKilometers = distance;
+        }
+        public EventMonitorState execute(final Event event, final Location location)
+        {
+            EventMonitorState res=this;
+            switch(this)
+            {
+                case IN_RANGE:
+                    if(null != event) {//only run if there is a valid object available
+                        if (new GeoPoint(location).distanceInKilometersTo(event.facility().location()) >=
+                                maxDistanceToEventInKilometers) {
+                            res = OUT_RANGE;
+                        } else {
+                            Log.d("spocht.eventMonitor", "Location still in range. Noop.");
+                        }
+                    }
+                    break;
+                case OUT_RANGE:
+                    if(null != event) {//only run if there is a valid object available
+                        if (new GeoPoint(location).distanceInKilometersTo(event.facility().location()) <=
+                                maxDistanceToEventInKilometers) {
+                            res = IN_RANGE;
+                        } else {
+                            Log.d("spocht.eventMonitor", "Location still in range. Noop.");
+                        }
+                    }
+                    break;
+                default:
+                    Log.d("spocht.eventMonitor","Reached default of switch case in an enum ...  something went wrong!");
+                    break;
+            }
+            return res;
+        }
+    }
+    private EventMonitorState state;
 
     public EventMonitor(Context ctx) {
+        state = EventMonitorState.OUT_RANGE;
         MyLocationListener.create(ctx);
         MyLocationListener.getInstance().register(
                 new LocationCallback<Void, Location>() {
                     @Override
                     public Void operate(Location location) {
                         if(null != event()) {//only run if there is a valid object available
-                            if (new GeoPoint(location).distanceInKilometersTo(event().facility().location()) >=
-                                    maxDistanceToEventInKilometers) {
-                                Log.d("spocht.eventMonitor", "Location too far away. Checking out.");
-                                event().checkOut(DataManager.getInstance().currentUser());
-                                setEvent(null);
-                            } else {
-                                Log.d("spocht.eventMonitor", "Location still in range. Noop.");
+                            EventMonitorState next = state.execute(event(),location);
+                            if(next != state)
+                            {
+                                switch(next)
+                                {
+                                    case IN_RANGE:
+                                        Log.d("spocht.eventMonitor","set foot in danger zone");
+                                        break;
+                                    case OUT_RANGE:
+                                        Log.d("spocht.eventMonitor", "Location too far away. Checking out.");
+                                        event().checkOut(DataManager.getInstance().currentUser());
+                                        setEvent(null);
+                                        break;
+                                }
                             }
                         }
                         return null;
