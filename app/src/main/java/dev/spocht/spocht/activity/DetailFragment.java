566d6363352aeba339206ba54c40d9d9a99a241d
Beat Luginbühl <lugi@lugipfupf.ch>
2015-09-14 11:58:11 +0200

    just using previously uploaded icon

--- app/src/main/java/dev/spocht/spocht/activity/DetailFragment.java
+++ app/src/main/java/dev/spocht/spocht/activity/DetailFragment.java
@@ -61,7 +61,8 @@ public class DetailFragment extends ListFragment {
         );
         params.addRule(RelativeLayout.ALIGN_PARENT_END);
         mNewGameImageButton.setLayoutParams(params);
-        mNewGameImageButton.setImageResource(R.drawable.ic_new_releases_white_24dp);
+        mNewGameImageButton.setImageResource(R.drawable.ic_plus_circle_outline_black_24dp);
+        mNewGameImageButton.setBackgroundColor(getResources().getColor(R.color.white));
 
         infoLayout.addView(mNewGameImageButton);
 
