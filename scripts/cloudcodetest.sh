432435fabbe2766ef8f6b5d66882705e98e77996
Rudolf Müller <rudolfj.mueller@web.de>
2015-09-22 15:01:50 +0200

    update test script to work with cygwin

--- scripts/cloudcodetest.sh
+++ scripts/cloudcodetest.sh
@@ -14,37 +14,37 @@ echo $eventId;
 echo "$eventId";
 
 #echo "CheckoutOrigUser"
-#retCheckoutOrigUser=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$origUserId'"}, "outcome" : {"value": "'$outcome'"}}' https://api.parse.com/1/functions/checkout`
+#retCheckoutOrigUser=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$origUserId'"}, "outcome" : {"value": "'$outcome'"}}' https://api.parse.com/1/functions/checkout`
 #echo $retCheckoutOrigUser
 #sleep 5
 
 
 echo "CheckinOtherUser"
-retCheckinOtherUser=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkin`
+retCheckinOtherUser=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkin`
 echo $retCheckinOtherUser
 sleep 5
 
 
 echo "CheckoutOtherUser"
-retCheckoutOrigUser=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkout`
+retCheckoutOrigUser=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkout`
 echo $retCheckoutOtherUser
 sleep 5
 
 
 echo "CheckinOtherUserAgain"
-retCheckinOtherUserAgain=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkin`
+retCheckinOtherUserAgain=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/checkin`
 echo $retCheckinOtherUserAgain
 sleep 5
 
 
 echo "StartGame"
-retStartGame=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/startGame`
+retStartGame=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/startGame`
 echo $retStartGame
 sleep 5
 
 
 echo "StopGame"
-retStopGame=`curl -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/stopGame`
+retStopGame=`curl -k -X POST  -H "X-Parse-Application-Id: IvP2CsQV7fRqfg0tSQs2Ugot9YCDo4VAdRUYsQFd"  -H "X-Parse-REST-API-Key: MfTV1x7f62HBQp0CMHj64dFpQBWZ3EcNpB2FnBtJ"  -H "Content-Type: application/json"  -d '{"event":{"id" : "'$eventId'"}, "user":{"id" : "'$otherUserId'"}, "outcome" : {"value": "'$outcome'"}}'  https://api.parse.com/1/functions/stopGame`
 echo $retStopGame
 sleep 5
 
